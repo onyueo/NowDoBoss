0aa62e8614ea1ccfcf63e8a45d7c176043f793ca
김광연 <kwang9705@naver.com>
2024-04-17 22:01:03 +0900

    [BE] feat: 유동인구, 매출DB insert, 통신 테스트
    Former-commit-id: 44a0c64834c949a7166462a9f2aa56c74dc0701f

---
+++ BackEnd/SpringBootServer/src/main/resources/db/data_sales_district.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:99af7547b50f5a18c50ebd42194da115d4239a201964cf6e12e9f5dabde7085b
+size 5974525
---
+++ BackEnd/SpringBootServer/src/main/resources/db/data_store_district.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7bcedd5a3256b962797d42b5cc0ce94d482e72a6991aaae50be082c7733583f
+size 4191666
