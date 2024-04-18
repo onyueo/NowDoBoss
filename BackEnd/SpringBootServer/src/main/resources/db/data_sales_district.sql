922863cc3b1e39c071e3447e70f69b44a7829621
김광연 <kwang9705@naver.com>
2024-04-18 10:47:58 +0900

    [BE] feat: 서비스 업종 타입 추가하기
    Former-commit-id: da20237819f0a7bcf14fc7f0f234798bdda36e0c

--- BackEnd/SpringBootServer/src/main/resources/db/data_sales_district.sql
+++ BackEnd/SpringBootServer/src/main/resources/db/data_sales_district.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99af7547b50f5a18c50ebd42194da115d4239a201964cf6e12e9f5dabde7085b
-size 5974525
+oid sha256:8edacd1addb349968642acc820c0a63dd741f7d8562e5b609e65ab825551980a
+size 5975636
