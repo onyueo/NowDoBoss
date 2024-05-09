b8702277a1683d1b05b94624dda57237566d7090
tlsehdrms124 <tlsehdrms124@naver.com>
2024-05-09 14:44:23 +0900

    [BE] feat: 배포서버용 db 마이그레이션 작업 (스키마 생성)
    Former-commit-id: e2e421c08bc97514fdc4131d1d603ea3fd87c365

--- BackEnd/SpringBootServer/src/main/resources/db/migration/V1__Initial_shema.sql
+++ BackEnd/SpringBootServer/src/main/resources/db/migration/V1__Initial_shema.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f135debb61464d2e22afade77b48a31f93d3c5b54b0243c712791b0f2cd7e286
-size 1290
+oid sha256:f386b3b42c49001832fc5da693c75fad4592531a8c5b796decf2fffd498eb950
+size 46770
