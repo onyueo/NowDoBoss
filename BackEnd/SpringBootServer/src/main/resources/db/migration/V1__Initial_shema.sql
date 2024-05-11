cab8cb7ead318fa1a4910356c9da2ca2116c80dc
tlsehdrms124 <tlsehdrms124@naver.com>
2024-05-11 20:24:30 +0900

    [BE] fix: 배포버전 db 마이그레이션 초기 설정 수정
    Former-commit-id: cf8d700e38c6ae62bc7b80e9fb63944b8d9bc09b
    Former-commit-id: 995fce2e883710447c541918c1974388592401a6

--- BackEnd/SpringBootServer/src/main/resources/db/migration/V1__Initial_shema.sql
+++ BackEnd/SpringBootServer/src/main/resources/db/migration/V1__Initial_shema.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f386b3b42c49001832fc5da693c75fad4592531a8c5b796decf2fffd498eb950
-size 46770
+oid sha256:fe510eea122789447be9454502b48a2b13c0785446a6ebcaeb89e699738dd4aa
+size 57202
