6bd3b53750fef117671f4a2af3c717c01d882245
tlsehdrms124 <tlsehdrms124@naver.com>
2024-05-11 21:02:52 +0900

    [BE] fix: 배포서버 db 마이그레이션 초기시 초기 스크립트 순서 재조정
    Former-commit-id: 9a275b657b4ef907d0b13046cc656bf40c381cc9
    Former-commit-id: 8bb7bb4ad2de15d277ceed3e9baf680c9e1bdfbc

--- BackEnd/SpringBootServer/src/main/resources/db/migration/V1__Initial_shema.sql
+++ BackEnd/SpringBootServer/src/main/resources/db/migration/V1__Initial_shema.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe510eea122789447be9454502b48a2b13c0785446a6ebcaeb89e699738dd4aa
-size 57202
+oid sha256:376184eff1002013c3fdc1c5ec84cb18b8bd13cecaf1cb289c6e87de6f8e847a
+size 55745
