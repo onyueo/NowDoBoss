94376dbbebfbea992cdf03f45a60542a76a39d2c
김온유 <gkc04075@gmail.com>
2024-05-20 03:42:55 +0900

    [FE] feat: 카카오톡 공유 상권분석 작업중
    Former-commit-id: ab1017403d8f2a44599327411e2380e0e7fbb445 [formerly 062595c75938b3f8558fd4e017e8b903cdfc95d1]
    Former-commit-id: d16b35d4d0d26d8bd3a7fc68ae26693dadfcd1c7
    Former-commit-id: e34919d670454828d7fbf3dcbad100c7fa599618

--- FrontEnd/src/types/KaKaoShareType.ts
+++ FrontEnd/src/types/KaKaoShareType.ts
@@ -13,3 +13,17 @@ export interface SimulationDataType {
   url: string
   input: SimulationData
 }
+
+// 상권분석 요청 데이터 타입
+export interface AnalysisData {
+  districtCode: number
+  administrationCode: number
+  commercialCode: number
+  serviceCode: string
+  periodCode: number | null
+}
+
+export interface AnalysisDataType {
+  url: string
+  input: AnalysisData
+}
